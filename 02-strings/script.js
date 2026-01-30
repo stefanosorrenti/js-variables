72308becced8f587243d6474cb0a3c4ac50f3f70
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-01-30 12:48:12 +0100

    Add 2th task - 02-strings

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -7,6 +7,14 @@ console.log(name)
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+const perfectNumber = 42;
+console.log(perfectNumber);
+
+const nameOfNumber = 'Quarantadue';
+console.log(nameOfNumber);
+
+const numbValue = true;
+console.log(numbValue);
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
